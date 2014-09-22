c39ee6bbb3a9e5fe215a52a05b78c485c031ecc8
Erik Ylipää <ylipaa@sics.se>
2014-09-22 13:57:36 +0200

    Added some clarifications

--- src/segment.py
+++ src/segment.py
@@ -8,13 +8,15 @@ class Segment:
     def __init__(self, mat_filename):
         """Creates a new segment object from the file named *mat_filename*"""
         #First extract the variable name of the struct, there should be exactly one struct in the file
-        [(struct, shape, dtype)] = scipy.io.whosmat(mat_filename)
+        [(struct_name, shape, dtype)] = scipy.io.whosmat(mat_filename)
         if dtype != 'struct':
             raise ValueError("File {} does not contain a struct".format(mat_filename))
 
         self.filename = os.path.basename(mat_filename)
         self.dirname = os.path.dirname(os.path.abspath(mat_filename))
-        self.name = struct
+        self.name = struct_name
+
+        #The matlab struct contains the variable mappings, we're only interested in the variable *self.name*
         self.mat_struct = scipy.io.loadmat(mat_filename, struct_as_record=False, squeeze_me=True)[self.name]
         self.dataframe = pandas.DataFrame(self.mat_struct.data.transpose(), columns=self.mat_struct.channels)
 
