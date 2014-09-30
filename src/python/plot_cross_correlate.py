64b3a228de60f00ad99eb0e115cfd5b1bc2402b9
Erik Ylipää <ylipaa@sics.se>
2014-09-30 18:06:30 +0200

    Added a plot function for generating boxplots over the time lags and their correlation values for different times

--- src/python/plot_cross_correlate.py
+++ src/python/plot_cross_correlate.py
@@ -218,6 +218,57 @@ def get_delta_t_distributions(file_correlations, segment_start=None, segment_end
     return interictal_delta_dist, preictal_delta_dist
 
 
+def boxplot_delta_t_distributions(file_correlations, output, channels_per_plot=1, **kwargs):
+    pp = PdfPages(output)
+
+    preictal_channel_corrs = defaultdict(lambda: defaultdict(list))
+    interictal_channel_corrs = defaultdict(lambda: defaultdict(list))
+
+    channels = set()
+    for f, correlations in file_correlations:
+        for channel_pair, frames in correlations.items():
+            channels.add(channel_pair)
+            for time_interval, time_corrlist in frames.items():
+                for (delta_t, corr) in time_corrlist:
+                    if 'preictal' in f.lower():
+                        preictal_channel_corrs[channel_pair][delta_t].append(corr)
+                    elif 'interictal' in f.lower():
+                        interictal_channel_corrs[channel_pair][delta_t].append(corr)
+
+   
+    for i, channel_pair in enumerate(sorted(channels)):
+        fig = plt.figure(dpi=300)
+
+        interictal_times, interictal_data = zip(*sorted(interictal_channel_corrs[channel_pair].items()))
+        preictal_times, preictal_data = zip(*sorted(preictal_channel_corrs[channel_pair].items()))
+
+        interictal_bp = plt.boxplot(interictal_data, positions=[i*5-1 for i in range(len(interictal_times))], sym='')
+        preictal_bp = plt.boxplot(preictal_data, positions=[i*5+1 for i in range(len(preictal_times))], sym='')
+
+        for box in interictal_bp['boxes']:
+            plt.setp(box, color='blue')
+
+        for box in preictal_bp['boxes']:
+            plt.setp(box, color='red')
+
+        plt.xticks([5*i for i in range(len(interictal_times))], interictal_times, fontsize=10)
+        plt.ylim(0,1)
+        fig.subplots_adjust(left=0.3, right=0.95, top=0.90)
+
+        # draw temporary red and blue lines and use them to create a legend
+        blue_line = matplotlib.lines.Line2D([], [], color='blue', label='Interictal')
+        red_line = matplotlib.lines.Line2D([], [], color='red', label='Preictal')
+        plt.legend((blue_line, red_line), ("Interictal", "Preictal"), bbox_to_anchor=(0., 1.02, 1., .102),
+                   loc=3, mode='expand', borderaxespad=0.0, ncol=2)
+
+        plt.ylabel("Correlation")
+        plt.xlabel("Time lag (s)")
+
+        pp.savefig(fig, dpi=300, papertype='a0')
+        plt.close(fig)
+    pp.close()
+
+
 def plot_delta_t_distributions(file_correlations, output, channels_per_plot=1, **kwargs):
     interictal_delta_dists, preictal_delta_dists = get_delta_t_distributions(file_correlations, **kwargs)
     figs = []
@@ -273,7 +324,7 @@ if __name__ == '__main__':
     parser.add_argument("--output", help="The output to plot to.", metavar="FILENAME")
     parser.add_argument("--segment-start", help="If this argument is supplied, only windows after this time in seconds will be used.", type=float, metavar="SECONDS")
     parser.add_argument("--segment-end", help="If this argument is supplied, only windows before this time will be used.", type=float, metavar="SECONDS")
-    parser.add_argument("--mode", help="Which plot should be used.", choices=["heatmap", "class_scatter", "boxplot", "delta_times", "delta_dists"])
+    parser.add_argument("--mode", help="Which plot should be used.", choices=["heatmap", "class_scatter", "boxplot", "delta_times", "delta_dists", "boxdelta"])
     parser.add_argument("--channels-per-plot", help="How many channels should each plot have", type=int)
     parser.add_argument("csv_files", help="The csv files to be plotted, chan be files, directories or combinations thereof", nargs="+")
     #parser.add_argument("--channels", help="Selects a subset of the channels to use.")
@@ -294,6 +345,8 @@ if __name__ == '__main__':
                   channels_per_plot=args.channels_per_plot)
     elif args.mode == 'delta_dists':
         plot_delta_t_distributions(correlations, output=args.output, segment_start=args.segment_start, segment_end=args.segment_end, channels_per_plot=args.channels_per_plot)
+    elif args.mode == 'boxdelta':
+        boxplot_delta_t_distributions(correlations, output=args.output, segment_start=args.segment_start, segment_end=args.segment_end, channels_per_plot=args.channels_per_plot)
 
 
 
