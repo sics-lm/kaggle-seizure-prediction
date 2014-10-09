850b7975a073f7ea1a1860bb8de55682714dd81d
Erik Ylipää <ylipaa@sics.se>
2014-10-09 16:16:21 +0200

    Added caching of models

--- src/r/classification_pipeline.r
+++ src/r/classification_pipeline.r
@@ -1,8 +1,18 @@
 source("correlation_convertion.r")
 source("seizure_modeling.r")
 
-runClassification <- function(featureFolder, rebuildData=FALSE, trainingRatio=.8) {
+runClassification <- function(featureFolder, rebuildData=FALSE, trainingRatio=.8, rebuildModel=FALSE, modelFile=NULL) {
     ## Runs the whole classification on the featureFolder
+    ## Args:
+    ##    featureFolder: a folder containing the feature csv files to train on
+    ##    reuildData: Logic flag whether to rebuild the feature dataframes. If false, cached versions will be used if they exist
+    ##    trainingRatio: the ratio of data to use for training the model. 1.0 uses almost all data for the training set (1 example be left in the test set)
+    ##    modelFile: Name of a model RDS file to use. If NULL and rebuildModel is FALSE, the newest model file in featureFolder will be used. If NULL and rebuildModel is TRUE, a name will be generated based on the model label in caret and the time the model was built. 
+    ##    rebuildModel: Logic flag whether to rebuild the model. If FALSE, the latest file with the suffix model_ in the feature folder will be used as the model
+    ## Returns:
+    ##    A list with (model, confMatrix, segmentClassification) for convenience. segmentClassification is a table of the original feature file names and a score based on how preictal they are. This data is automatically saved to a CSV file during the run of the function.
+    ##
+    
     dataSet <- loadDataFrames(featureFolder, rebuildData=rebuildData)
     interictal <- dataSet[[1]]
     preictal <- dataSet[[2]]
@@ -14,16 +24,41 @@ runClassification <- function(featureFolder, rebuildData=FALSE, trainingRatio=.8
                                            trainingPerc=trainingRatio)
     trainingData <- experimentSplit[[1]]
     testData <- experimentSplit[[2]]
-    model <- trainModel(trainingData)
 
-    ## ### Save the model to an rds ## ##
-    modelLabel <- model$modelInfo$label
-    modelTime <- format(Sys.time(), "%Y-%m-%d-%H:%M:%S")
-    modelFileName <- sprintf("model_%s_%s.rds", modelLabel, modelTime)
-    modelPath <- file.path(featureFolder, modelFileName)
+    if (is.null(modelFile)) {
+        ## Read the first available model
+        modelFiles <- file.path(featureFolder,
+                                list.files(featureFolder,
+                                           pattern="model_.*\\.rds"))
+        if (length(modelFiles) > 0) {
+            fileInfos <- file.info(modelFiles)
+            fileDates <- data.frame(ctime=fileInfos$ctime,
+                                    name=row.names(fileInfos),
+                                    stringsAsFactors=FALSE)
+            latestDate <- max(fileDates$ctime)
+            modelFile <- fileDates[fileDates$ctime==latest,]$name[[1]]
+        }
+        else {
+            rebuildModel <- TRUE
+        }
+    }
 
-    saveRDS(model, modelPath)
-    ## ## Done saving model ## ##
+    if (rebuildModel) {
+        model <- trainModel(trainingData)
+
+        if (is.null(modelFile)) {
+            modelLabel <- model$modelInfo$label
+            modelTime <- format(Sys.time(), "%Y-%m-%d-%H:%M:%S")
+            modelFileName <- sprintf("model_%s_%s.rds",
+                                     modelLabel,
+                                     modelTime)
+            modelFile <- file.path(featureFolder, modelFileName)
+        }
+        saveRDS(model, modelFile)
+    }
+    else {
+        model <- readRDS(modelFile)
+    }
 
     segmentClassification <- assignSegmentProbability(model, unlabeledTests)
     ## ## Saving segment class probabilities ## ##
