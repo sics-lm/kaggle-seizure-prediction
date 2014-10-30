d693090f195e2c6104e27d7d4a66ccf42272dfe2
Erik Ylipää <ylipaa@sics.se>
2014-10-30 18:01:59 +0100

    Added a score normalization which standardizes the scores by subtracting the mean and scale with the standard deviation.

--- src/python/collect_classification_scores.py
+++ src/python/collect_classification_scores.py
@@ -8,6 +8,7 @@ import re
 import sys
 import datetime
 
+import numpy as np
 from collections import defaultdict
 
 def collect_scores(score_files, only_newest=True):
@@ -64,7 +65,7 @@ def fix_segment_names(scores,
     return new_names
 
 
-def normalize_scores(all_scores, pattern=r"([A-Za-z]*_[0-9])*"):
+def normalize_scores(all_scores, pattern=r"([A-Za-z]*_[0-9])*", old_normalization=True):
     """
     'Normalizes' the scores for the different subjects, so that their score are in the interval [0,1]. This is done on a per-subject basis.
     The subjects are divided by their prefix, given by the first match group of the regular expression *pattern*.
@@ -83,18 +84,36 @@ def normalize_scores(all_scores, pattern=r"([A-Za-z]*_[0-9])*"):
 
     normalized_scores = dict()
     for subject, scores in subject_scores.items():
-        max_score = max(scores)
-        min_score = min(scores)
-        if max_score > 0:
-            for segment in subject_segments[subject]:
-                segment_score = all_scores[segment]
-                segment_score -= min_score
-                segment_score /= float(max_score)
-                normalized_scores[segment] = segment_score
+        if old_normalization:
+            max_score = max(scores)
+            min_score = min(scores)
+            if max_score > 0:
+                for segment in subject_segments[subject]:
+                    segment_score = all_scores[segment]
+                    segment_score -= min_score
+                    segment_score /= float(max_score)
+                    normalized_scores[segment] = segment_score
+        else:
+            print("Using new normalization")
+            score_mean = np.mean(scores)
+            score_std = np.std(scores)
+            if score_mean > 0 and score_std > 0:
+                for segment in subject_segments[subject]:
+                    segment_score = all_scores[segment]
+                    segment_score -= score_mean
+                    segment_score /= score_std
+                    segment_score += 0.5
+                    if segment_score < 0:
+                        segment_score = 0
+                    elif segment_score > 1:
+                        segment_score = 1
+                    print("Normalized score is: ", segment_score)
+                    normalized_scores[segment] = segment_score
+
     return normalized_scores
 
 
-def get_scores(root, normalize=False):
+def get_scores(root, normalize=False, old_normalization=True):
     """
     Gathers all the scores in subdirectories of the folder *root*
     """
@@ -103,14 +122,14 @@ def get_scores(root, normalize=False):
     fixed_scores = fix_segment_names(collected_scores)
 
     if normalize:
-        fixed_scores = normalize_scores(fixed_scores)
+        fixed_scores = normalize_scores(fixed_scores, old_normalization=old_normalization)
 
 
     return fixed_scores
 
 
-def write_scores(root, output, normalize=False):
-    scores = get_scores(root, normalize=normalize)
+def write_scores(root, output, normalize=False, new_normalization=False):
+    scores = get_scores(root, normalize=normalize, old_normalization=not new_normalization)
     csv_writer = csv.writer(output)
     csv_writer.writerow(['clip', 'preictal'])
     for segment_name, score in sorted(scores.items()):
@@ -127,6 +146,9 @@ if __name__ == '__main__':
     parser.add_argument("-n", "--normalize",
                         help="Normalize the scores on a per-subject basis to the range [0,1]",
                         action='store_true')
+    parser.add_argument("--new-normalization",
+                        help="Use the new normalization where we standarize the per subject scores by subtracting the mean, dividing by the std, shift +0.5 and clip at 0 and 1.",
+                        action='store_true')
 
     # parser.add_argument("--match", help="A unix shell style expression used to match the files in the subdirectories of root")
     args = parser.parse_args()
@@ -142,7 +164,7 @@ if __name__ == '__main__':
             output = open(args.output, 'w')
 
     try:
-        write_scores(args.root, output, args.normalize)
+        write_scores(args.root, output, args.normalize, args.new_normalization)
 
     finally:
         if args.output:
