d042e9fd90100cd6f5c1d3034d4139aa8112ca79
Erik Ylipää <ylipaa@sics.se>
2014-10-31 11:49:29 +0100

    Added command line arguments

--- src/python/pca_analysis.py
+++ src/python/pca_analysis.py
@@ -96,3 +96,59 @@ def pca_transform(interictal, preictal, test_data, label=None, do_standardize=Fa
     plt.title('Transformed samples with class labels from matplotlib.mlab.PCA()')
 
     return fig, pca
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description="""Script for creating PCA plots of data""")
+
+    parser.add_argument("feature_folder",
+                        help="""The folder containing the features""",)
+    # parser.add_argument("--rebuild-data",
+    #                     action='store_true',
+    #                     help="Should the dataframes be re-read from the csv feature files",
+    #                     dest='rebuild_data')
+    parser.add_argument("--no-downsample",
+                        action='store_false',
+                        default=True,
+                        help="Disable downsampling of the majority class",
+                        dest='do_downsample')
+    parser.add_argument("--n-samples",
+                        default=300,
+                        type=int,
+                        help="The number of samples to take from each dataset when downsampling",
+                        dest='n_samples')
+    parser.add_argument("--do-standardize",
+                        default=False,
+                        action='store_true',
+                        help="Should the columns of the data be standardized (zero mean, standard deviation 1)",
+                        dest='do_standardize')
+
+    # parser.add_argument("--no-segment-split",
+    #                     action='store_false',
+    #                     help="Disable splitting data by segment.",
+    #                     dest='do_segment_split',
+    #                     default=True)
+    # parser.add_argument("--processes",
+    #                     help="How many processes should be used for parellelized work.",
+    #                     dest='processes',
+    #                     default=4,
+    #                     type=int)
+    parser.add_argument("--frame-length",
+                        help="The size in windows each frame (feature vector) should be. Only applicable to --feature-type 'xcorr' at the momen",
+                        dest='frame_length',
+                        default=1,
+                        type=int)
+    parser.add_argument("--feature-type", help="What kind of features are being processed.", choices=['wavelets', 'xcorr'],
+                        default='wavelets')
+
+    args = parser.parse_args()
+    if args.feature_type == 'wavelets':
+        run_pca_analysis(args.feature_folder, do_downsample=args.do_downsample,
+                         n_samples=args.n_samples, do_standardize=args.do_standardize)
+    elif args.feature_type == 'xcorr':
+        run_xcorr_pca_analysis(feature_folder=args.feature_folder,
+                               frame_length=args.frame_length,
+                               do_downsample=args.do_downsample,
+                               n_samples=args.n_samples,
+                               do_standardize=args.do_standardize)
