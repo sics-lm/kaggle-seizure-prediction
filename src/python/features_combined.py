08d315cbd1637fa7338ab734fc63181ad4f94a3b
Erik Ylipää <ylipaa@sics.se>
2014-11-07 15:11:05 +0100

    Added the module which deals with loading combined feature files

---
+++ src/python/features_combined.py
@@ -0,0 +1,22 @@
+"""
+Module for dealing with combined features
+"""
+import pandas as pd
+
+import wavelet_classification
+import correlation_convertion
+
+
+def load(segment_files, **kwargs):
+    """Loads the multiple features from segment_files and concatenate them to a single dataframe. The feature loader to use is based on the file name. If the path contains 'wavelet', the wavelet feature loader will be used. If it contains 'corr' the cross-correlation feature loader will be used."""
+
+    print("Loading files with kwargs: ", kwargs)
+    dataframes = []
+    for segment_file in segment_files:
+        if 'wavelet' in segment_file:
+            dataframes.append(wavelet_classification.load_csv(segment_file))
+        elif 'corr' in segment_file:
+            dataframes.append(correlation_convertion.load_and_pivot(segment_file))
+        else:
+            raise NotImplementedError("Don't know which feature loading function to use for {}.".format(segment_file))
+    return pd.concat(dataframes, axis=1)
