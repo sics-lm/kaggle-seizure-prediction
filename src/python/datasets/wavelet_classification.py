50efd6a957cd3486c2fea3b7078818dfa27452d7
Erik Ylipää <ylipaa@sics.se>
2014-12-12 16:03:27 +0100

    pep8 and docstrings.

--- src/python/datasets/wavelet_classification.py
+++ src/python/datasets/wavelet_classification.py
@@ -8,13 +8,20 @@ import numpy as np
 
 
 def load_csv(filename, frame_length=12, sliding_frames=False):
+    """
+    Loads the wavelet of hills features from the given filename.
+    :param filename: The filename to load features from.
+    :param frame_length: The desired frame length in windows to use.
+    :param sliding_frames: If True, the data will be extended by using sliding frames of the feature windows.
+    :return: A DataFrame with the loaded features.
+    """
 
     # Read the csv file with pandas and extract the values into an numpy array
     from_file_array = pd.read_table(filename, sep=',', dtype=np.float64, header=None).values
 
     # Assert that the csvfiles contain frames consisting 12 windows.
     assert_msg = 'file: "{}" does not have a column count divisible by 12 since it is: {}.'
-    assert (from_file_array.shape[1] % 12) == 0, assert_msg.format(filename,from_file_array.shape[1])
+    assert (from_file_array.shape[1] % 12) == 0, assert_msg.format(filename, from_file_array.shape[1])
 
     # Number of windows in the csv frame
     window_size = from_file_array.shape[1] / 12
@@ -22,7 +29,7 @@ def load_csv(filename, frame_length=12, sliding_frames=False):
     n_rows = from_file_array.shape[0]*12
 
     # Reshaped array an array which is one windows wide and n_windows long.
-    reshaped_array = from_file_array.reshape(n_rows,window_size)
+    reshaped_array = from_file_array.reshape(n_rows, window_size)
 
     # Extract this function out into its own file and use it also with the cross correlation frames
     if sliding_frames:
@@ -30,7 +37,7 @@ def load_csv(filename, frame_length=12, sliding_frames=False):
     else:
         n_frames = reshaped_array.shape[0]/frame_length
         frame_size = window_size*frame_length
-        return pd.DataFrame(data=reshaped_array.reshape(n_frames,frame_size))
+        return pd.DataFrame(data=reshaped_array.reshape(n_frames, frame_size))
 
 
 def load_data_frames(feature_folder,
